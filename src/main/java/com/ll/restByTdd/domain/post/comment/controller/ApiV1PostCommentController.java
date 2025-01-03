0a5db79e52f2f92911d02877f89a8c35a8589202
jhs512 <jangka512@gmail.com>
2025-01-03 15:44:37 +0900

    t, g, PUT /api/v1/posts/1/comments/1

--- src/main/java/com/ll/restByTdd/domain/post/comment/controller/ApiV1PostCommentController.java
+++ src/main/java/com/ll/restByTdd/domain/post/comment/controller/ApiV1PostCommentController.java
@@ -62,4 +62,39 @@ public class ApiV1PostCommentController {
                 "%d번 댓글이 삭제되었습니다.".formatted(id)
         );
     }
+
+
+    record PostCommentModifyReqBody(
+            @NotBlank
+            @Length(min = 2, max = 100)
+            String content
+    ) {
+    }
+
+    @PutMapping("/{id}")
+    public RsData<PostCommentDto> modify(
+            @PathVariable long postId,
+            @PathVariable long id,
+            @RequestBody @Valid PostCommentModifyReqBody reqBody
+    ) {
+        Member actor = rq.checkAuthentication();
+
+        Post post = postService.findById(postId).orElseThrow(
+                () -> new ServiceException("404-1", "%d번 글은 존재하지 않습니다.".formatted(postId))
+        );
+
+        PostComment postComment = post.getCommentById(id).orElseThrow(
+                () -> new ServiceException("404-2", "%d번 댓글은 존재하지 않습니다.".formatted(id))
+        );
+
+        postComment.checkActorCanModify(actor);
+
+        postComment.modify(reqBody.content);
+
+        return new RsData<>(
+                "200-1",
+                "%d번 댓글이 수정되었습니다.".formatted(id),
+                new PostCommentDto(postComment)
+        );
+    }
 }
