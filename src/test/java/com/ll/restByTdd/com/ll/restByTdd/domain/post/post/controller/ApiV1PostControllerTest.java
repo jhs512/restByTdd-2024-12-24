222af64d7a4097a8dc8ad46b6152d425045fbfae
jhs512 <jangka512@gmail.com>
2025-01-03 11:52:38 +0900

    page 2

--- src/test/java/com/ll/restByTdd/com/ll/restByTdd/domain/post/post/controller/ApiV1PostControllerTest.java
+++ src/test/java/com/ll/restByTdd/com/ll/restByTdd/domain/post/post/controller/ApiV1PostControllerTest.java
@@ -465,7 +465,7 @@ public class ApiV1PostControllerTest {
     void t17() throws Exception {
         ResultActions resultActions = mvc
                 .perform(
-                        get("/api/v1/posts")
+                        get("/api/v1/posts?page=1&pageSize=3")
                 )
                 .andDo(print());
 
@@ -474,7 +474,7 @@ public class ApiV1PostControllerTest {
                 .andExpect(handler().methodName("items"))
                 .andExpect(status().isOk());
 
-        List<Post> posts = postService.findAllByOrderByIdDesc();
+        List<Post> posts = postService.findByListedPaged(true, 1, 3);
 
         for (int i = 0; i < posts.size(); i++) {
             Post post = posts.get(i);
